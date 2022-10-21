ef5d05e70229f1c6314ed76fff4733f4ae903bfe
melissa98m <melissa.mangione@gmail.com>
2022-10-21 09:50:57 +0200

    add UserController

---
+++ go-with-dog-api/app/Http/Controllers/API/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json([
+            'status' => 'Success',
+            'data' => $users
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+}
