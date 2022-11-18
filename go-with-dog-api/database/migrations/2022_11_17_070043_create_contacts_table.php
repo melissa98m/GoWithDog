fa11e3c9bdf371f47698d3def5a43bb4d20c73b0
melissa98m <melissa.mangione@gmail.com>
2022-11-18 15:24:57 +0100

    laravel : modification

---
+++ go-with-dog-api/database/migrations/2022_11_17_070043_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->string('email');
+            $table->string('subject')->nullable();
+            $table->longText('contenu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
