9f7a6ce95652dbd0947e10a1363bdfbd91f289ab
melissa98m <melissa.mangione@gmail.com>
2023-02-03 11:45:49 +0100

    add place add by current user

---
+++ go-with-dog-app/src/component/account/account.js
@@ -0,0 +1,115 @@
+import React, {useEffect, useState} from "react";
+import { Box, CardHeader, Container, Card, TableContainer, Table, TablePagination } from "@mui/material";
+import axios from "axios";
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import LoupeIcon from '@mui/icons-material/Loupe';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import AdminMessage from "../../services/auth/adminMessage";
+import Grid from '@mui/material/Grid';
+import DisplayBallade from "../ballade/displayBallade";
+import DisplayPlace from "../place/displayPlace";
+
+
+
+
+function Account() {
+
+    document.title = "Mon compte";
+
+    const [user, setUser] = useState(null); // array of data
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [places , setPlaces] = useState([]);
+    const [data, setData] = useState(null);
+
+
+
+       const handleDataChange = async (dataChange) => {
+            await setUser(dataChange)
+        }
+
+
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/current-user'
+        ,{"headers" : { "Authorization":"Bearer"+localStorage.getItem('access_token') } }).then((actualData) => {
+            //actualData = actualData.data;
+            setLoading(true)
+            setUser(actualData.data);
+            getPlaces();
+            setError(null);
+        }).catch((err) => {
+            setError(err.message);
+            setUser(null);
+        }).finally(() => {
+            setLoading(false);
+        });
+    }, []);
+
+    let getPlaces = async () => {
+            await axios.get("http://127.0.0.1:8000/api/places-user" ,
+            {"headers" : { "Authorization":"Bearer"+localStorage.getItem('access_token') } }).then((actualData) => {
+            setPlaces(actualData.data.data)
+            })
+            }
+
+    return <Container maxWidth="xl" id='home'>
+
+        <Typography variant="h2" sx={{textAlign: "center" , marginTop: "35px"}} gutterBottom>Mon compte</Typography>
+        {loading ? (
+            <Typography variant="body" sx={{textAlign: "center"}} gutterBottom>Chargement de votre compte</Typography>
+        ) : (
+            <Box sx={{textAlign: "center" , marginTop: "5%" ,  marginBottom: "5%" , marginLeft: "auto" , marginRight: "auto" , maxWidth: "70%" ,
+                              backgroundColor: "#B1B3C1" , borderRadius: "10px" , marginBottom: "10px" , padding : "10px"}}>
+                              <Typography variant="h3" sx={{ textAlign: "center" , fontSize: "30px" }}>Informations personnelles</Typography><br/>
+                      <Typography variant="body" sx={{ marginTop: "10px" }}><strong> Username : </strong>{user.username}</Typography><br/>
+                      <Typography variant="body" sx={{ marginTop: "10px" }}><strong> Email : </strong>{user.email}</Typography><br/>
+                      <Typography variant="body" sx={{ marginTop: "10px" }}><strong> Date de création : </strong>{user.created_at.slice(0,10)}</Typography><br/>
+           </Box>
+        )
+        }
+         <Typography variant="h4" sx={{textAlign: "left"}} >Places que vous avez créée</Typography>
+        {loading ? (
+                    <Typography variant="body" sx={{textAlign: "center"}} gutterBottom>Chargement des places...</Typography>
+                ) : (
+                    <Box sx={{ maxWidth: '100%' }}>
+                                {places.map(({id, place_name, place_description , place_image, category, address}) => {
+                        return (
+                       <Card sx={{ display: 'inline-block' }}>
+                                            <CardMedia
+                                                component="img"
+                                                height="140"
+                                                src={`http://127.0.0.1:8000/storage/uploads/places/${place_image}`}
+                                                alt={place_name}
+                                            />
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    {place_name}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    {place_description.slice(0,30)}
+                                                </Typography>
+                                            </CardContent>
+                                              <CardActions>
+                                                    <Box sx={{display: 'flex', justifyContent: 'right'}}>
+                                                        <DisplayPlace DisplayPlaceValue={{id, place_name, place_description, place_image, category, address, data}} handleDataChange={handleDataChange} />
+                                                    </Box>
+                                                </CardActions>
+                                        </Card>
+
+                    )
+                })}
+                    </Box>
+
+
+                )
+                }
+
+</Container>
+
+}
+export default Account;
