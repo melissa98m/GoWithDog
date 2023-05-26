0475df03fdc104632c8bb9ddac9131ec647a9722
melissa98m <melissa.mangione@gmail.com>
2023-05-26 08:13:08 +0200

    place test

--- go-with-dog-api/tests/Unit/PlaceTest.php
+++ go-with-dog-api/tests/Unit/PlaceTest.php
@@ -11,32 +11,10 @@ class PlaceTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function it_belongs_to_a_user()
+    public function test_places_listed_successfully()
     {
-        $user = User::factory()->create();
-        $place = Place::factory()->create(['user' => $user->id]);
-
-        $this->assertInstanceOf(User::class, $place->user);
-        $this->assertEquals($user->id, $place->user->id);
+        $this->json('GET', 'api/places', ['Accept' => 'application/json'])
+            ->assertStatus(200);
     }
 
-    /** @test */
-    public function it_belongs_to_an_address()
-    {
-        $address = Address::factory()->create();
-        $place = Place::factory()->create(['address' => $address->id]);
-
-        $this->assertInstanceOf(Address::class, $place->address);
-        $this->assertEquals($address->id, $place->address->id);
-    }
-
-    /** @test */
-    public function it_belongs_to_a_category()
-    {
-        $category = Category::factory()->create();
-        $place = Place::factory()->create(['category' => $category->id]);
-
-        $this->assertInstanceOf(Category::class, $place->category);
-        $this->assertEquals($category->id, $place->category->id);
-    }
 }
